73a847c5343df143332a5d436720eaa845995951
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2018-01-25 03:01:01 +0000

    autoupdated protocol.json & generated files

---
+++ gen-src/ChromeDevtoolsProtocol/Model/Inspector/TargetReloadedAfterCrashEvent.php
@@ -0,0 +1,25 @@
+<?php
+namespace ChromeDevtoolsProtocol\Model\Inspector;
+
+/**
+ * Fired when debugging target has reloaded after crash
+ *
+ * @generated This file has been auto-generated, do not edit.
+ *
+ * @author Jakub Kulhan <jakub.kulhan@gmail.com>
+ */
+final class TargetReloadedAfterCrashEvent implements \JsonSerializable
+{
+	public static function fromJson($data)
+	{
+		$instance = new static();
+		return $instance;
+	}
+
+
+	public function jsonSerialize()
+	{
+		$data = new \stdClass();
+		return $data;
+	}
+}
