319e3f4f29a13ab260794c99058c361870453278
Mike Cochrane <mike.cochrane@airnz.co.nz>
2018-02-01 22:05:42 +0000

    Make sure we recieved something before attempting to iterate over it

--- src/ChromeDevtoolsProtocol/DevtoolsClient.php
+++ src/ChromeDevtoolsProtocol/DevtoolsClient.php
@@ -17,195 +17,201 @@ use Wrench\Payload\Payload;
 class DevtoolsClient implements DevtoolsClientInterface, InternalClientInterface
 {
 
-	use DevtoolsClientTrait;
+    use DevtoolsClientTrait;
 
-	/** @var callable[][] */
-	private $listeners = [];
+    /** @var callable[][] */
+    private $listeners = [];
 
-	/** @var Client|null */
-	private $wsClient;
+    /** @var Client|null */
+    private $wsClient;
 
-	/** @var int */
-	private $messageId = 0;
+    /** @var int */
+    private $messageId = 0;
 
-	/** @var object[] */
-	private $commandResults = [];
+    /** @var object[] */
+    private $commandResults = [];
 
-	/** @var object[][] */
-	private $eventBuffers = [];
+    /** @var object[][] */
+    private $eventBuffers = [];
 
-	public function __construct(string $wsUrl)
-	{
-		$this->wsClient = new WebSocketClient($wsUrl, "http://" . parse_url($wsUrl, PHP_URL_HOST));
-		if (!$this->wsClient->connect()) {
-			throw new RuntimeException(sprintf("Could not connect to [%s].", $wsUrl));
-		}
-	}
+    public function __construct(string $wsUrl)
+    {
+        $this->wsClient = new WebSocketClient($wsUrl, "http://" . parse_url($wsUrl, PHP_URL_HOST));
+        if (!$this->wsClient->connect()) {
+            throw new RuntimeException(sprintf("Could not connect to [%s].", $wsUrl));
+        }
+    }
 
-	public function __destruct()
-	{
-		if ($this->wsClient !== null) {
-			throw new LogicException(sprintf(
-				"You must call [%s::%s] method to release underlying WebSocket connection.",
-				__CLASS__,
-				"close"
-			));
-		}
-	}
+    public function __destruct()
+    {
+        if ($this->wsClient !== null) {
+            throw new LogicException(sprintf(
+                "You must call [%s::%s] method to release underlying WebSocket connection.",
+                __CLASS__,
+                "close"
+            ));
+        }
+    }
 
-	public function close(): void
-	{
-		$wsClient = $this->wsClient;
-		$this->wsClient = null;
-		$wsClient->disconnect();
-	}
+    public function close(): void
+    {
+        $wsClient = $this->wsClient;
+        $this->wsClient = null;
+        $wsClient->disconnect();
+    }
 
-	/**
-	 * @internal
-	 */
-	public function executeCommand(ContextInterface $ctx, string $method, $parameters)
-	{
-		$messageId = ++$this->messageId;
+    /**
+     * @internal
+     */
+    public function executeCommand(ContextInterface $ctx, string $method, $parameters)
+    {
+        $messageId = ++$this->messageId;
 
-		$payload = new \stdClass();
-		$payload->id = $messageId;
-		$payload->method = $method;
-		$payload->params = $parameters;
+        $payload = new \stdClass();
+        $payload->id = $messageId;
+        $payload->method = $method;
+        $payload->params = $parameters;
 
-		$this->getWsClient()->setDeadline($ctx->getDeadline());
-		$this->getWsClient()->sendData(json_encode($payload));
+        $this->getWsClient()->setDeadline($ctx->getDeadline());
+        $this->getWsClient()->sendData(json_encode($payload));
 
-		for (; ;) {
-			$this->getWsClient()->setDeadline($ctx->getDeadline());
-			foreach ($this->getWsClient()->receive() as $payload) {
-				/** @var Payload $payload */
-				$message = json_decode($payload->getPayload());
-				$this->handleMessage($message);
-			}
+        for (; ;) {
+            $this->getWsClient()->setDeadline($ctx->getDeadline());
+            $payloads = $this->getWsClient()->receive();
+            if ($payloads) {
+                foreach ($payloads as $payload) {
+                    /** @var Payload $payload */
+                    $message = json_decode($payload->getPayload());
+                    $this->handleMessage($message);
+                }
+            }
 
-			if (isset($this->commandResults[$messageId])) {
-				$result = $this->commandResults[$messageId];
-				unset($this->commandResults[$messageId]);
-				return $result;
-			}
-		}
-	}
+            if (isset($this->commandResults[$messageId])) {
+                $result = $this->commandResults[$messageId];
+                unset($this->commandResults[$messageId]);
+                return $result;
+            }
+        }
+    }
 
-	/**
-	 * @internal
-	 */
-	public function awaitEvent(ContextInterface $ctx, string $method)
-	{
-		if (!empty($this->eventBuffers[$method])) {
-			return array_shift($this->eventBuffers[$method])->params;
-		}
+    /**
+     * @internal
+     */
+    public function awaitEvent(ContextInterface $ctx, string $method)
+    {
+        if (!empty($this->eventBuffers[$method])) {
+            return array_shift($this->eventBuffers[$method])->params;
+        }
 
-		$this->eventBuffers = [];
+        $this->eventBuffers = [];
 
-		for (; ;) {
-			$eventMessage = null;
+        for (; ;) {
+            $eventMessage = null;
 
-			$this->getWsClient()->setDeadline($ctx->getDeadline());
-			foreach ($this->getWsClient()->receive() as $payload) {
-				/** @var Payload $payload */
-				$message = json_decode($payload->getPayload());
+            $this->getWsClient()->setDeadline($ctx->getDeadline());
+            $payloads = $this->getWsClient()->receive();
+            if ($payloads) {
+                foreach ($payloads as $payload) {
+                    /** @var Payload $payload */
+                    $message = json_decode($payload->getPayload());
 
-				$nextEventMessage = $this->handleMessage($message, $eventMessage === null ? $method : null);
+                    $nextEventMessage = $this->handleMessage($message, $eventMessage === null ? $method : null);
 
-				if ($nextEventMessage !== null) {
-					$eventMessage = $nextEventMessage;
-				}
-			}
+                    if ($nextEventMessage !== null) {
+                        $eventMessage = $nextEventMessage;
+                    }
+                }
+            }
 
-			if ($eventMessage !== null) {
-				return $eventMessage->params;
-			}
-		}
-	}
+            if ($eventMessage !== null) {
+                return $eventMessage->params;
+            }
+        }
+    }
 
-	private function handleMessage($message, ?string $returnIfEventMethod = null)
-	{
-		if (isset($message->error)) {
-			throw new ErrorException($message->error->message, $message->error->code);
+    private function handleMessage($message, ?string $returnIfEventMethod = null)
+    {
+        if (isset($message->error)) {
+            throw new ErrorException($message->error->message, $message->error->code);
 
-		} else if (isset($message->method)) {
-			if (isset($this->listeners[$message->method])) {
-				foreach ($this->listeners[$message->method] as $callback) {
-					$callback($message->params);
-				}
-			}
+        } else if (isset($message->method)) {
+            if (isset($this->listeners[$message->method])) {
+                foreach ($this->listeners[$message->method] as $callback) {
+                    $callback($message->params);
+                }
+            }
 
-			if ($returnIfEventMethod !== null && $message->method === $returnIfEventMethod) {
-				return $message;
-			} else {
-				if (!isset($this->eventBuffers[$message->method])) {
-					$this->eventBuffers[$message->method] = [];
-				}
-				array_push($this->eventBuffers[$message->method], $message);
-			}
+            if ($returnIfEventMethod !== null && $message->method === $returnIfEventMethod) {
+                return $message;
+            } else {
+                if (!isset($this->eventBuffers[$message->method])) {
+                    $this->eventBuffers[$message->method] = [];
+                }
+                array_push($this->eventBuffers[$message->method], $message);
+            }
 
-		} else if (isset($message->id)) {
-			$this->commandResults[$message->id] = $message->result ?? new \stdClass();
+        } else if (isset($message->id)) {
+            $this->commandResults[$message->id] = $message->result ?? new \stdClass();
 
-		} else {
-			throw new RuntimeException(sprintf(
-				"Unhandled message: %s",
-				json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
-			));
-		}
+        } else {
+            throw new RuntimeException(sprintf(
+                "Unhandled message: %s",
+                json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
+            ));
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	private function getWsClient()
-	{
-		if ($this->wsClient === null) {
-			throw new ClientClosedException("Client has been closed.");
-		}
+    private function getWsClient()
+    {
+        if ($this->wsClient === null) {
+            throw new ClientClosedException("Client has been closed.");
+        }
 
-		return $this->wsClient;
-	}
+        return $this->wsClient;
+    }
 
-	/**
-	 * @internal
-	 */
-	public function addListener(string $method, callable $listener): SubscriptionInterface
-	{
-		if (!isset($this->listeners[$method])) {
-			$this->listeners[$method] = [];
-		}
+    /**
+     * @internal
+     */
+    public function addListener(string $method, callable $listener): SubscriptionInterface
+    {
+        if (!isset($this->listeners[$method])) {
+            $this->listeners[$method] = [];
+        }
 
-		$this->listeners[$method][] = $listener;
+        $this->listeners[$method][] = $listener;
 
-		$callback = function () use ($method, $listener) {
-			foreach ($this->listeners[$method] as $k => $candidateListener) {
-				if ($candidateListener === $listener) {
-					unset($this->listeners[$method][$k]);
-					break;
-				}
-			}
+        $callback = function () use ($method, $listener) {
+            foreach ($this->listeners[$method] as $k => $candidateListener) {
+                if ($candidateListener === $listener) {
+                    unset($this->listeners[$method][$k]);
+                    break;
+                }
+            }
 
-			if (empty($this->listeners[$method])) {
-				unset($this->listeners[$method]);
-			}
-		};
+            if (empty($this->listeners[$method])) {
+                unset($this->listeners[$method]);
+            }
+        };
 
-		return new class($callback) implements SubscriptionInterface
-		{
-			/** @var callable */
-			private $callback;
+        return new class($callback) implements SubscriptionInterface
+        {
+            /** @var callable */
+            private $callback;
 
-			public function __construct(callable $callable)
-			{
-				$this->callback = $callable;
-			}
+            public function __construct(callable $callable)
+            {
+                $this->callback = $callable;
+            }
 
-			public function cancel(): void
-			{
-				$callback = $this->callback;
-				$callback();
-			}
-		};
-	}
+            public function cancel(): void
+            {
+                $callback = $this->callback;
+                $callback();
+            }
+        };
+    }
 
 }
