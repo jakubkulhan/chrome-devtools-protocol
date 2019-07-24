d4322652a3c8cd729fef60e22fc200e2c53ac3c4
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2019-07-24 03:01:21 +0000

    autoupdated protocol.json & generated files

---
+++ gen-src/ChromeDevtoolsProtocol/Model/SystemInfo/ImageTypeEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ChromeDevtoolsProtocol\Model\SystemInfo;
+
+/**
+ * Image format of a given image.
+ *
+ * @generated This file has been auto-generated, do not edit.
+ *
+ * @author Jakub Kulhan <jakub.kulhan@gmail.com>
+ */
+final class ImageTypeEnum
+{
+	const JPEG = 'jpeg';
+	const WEBP = 'webp';
+	const UNKNOWN = 'unknown';
+}
