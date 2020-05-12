72478b9c9f4e885d1d3af7244e7a773a669295ef
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2020-05-12 03:01:48 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Audits/SameSiteCookieWarningReasonEnum.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Audits/SameSiteCookieWarningReasonEnum.php
@@ -14,10 +14,9 @@ final class SameSiteCookieWarningReasonEnum
 	const WARN_SAME_SITE_UNSPECIFIED_CROSS_SITE_CONTEXT = 'WarnSameSiteUnspecifiedCrossSiteContext';
 	const WARN_SAME_SITE_NONE_INSECURE = 'WarnSameSiteNoneInsecure';
 	const WARN_SAME_SITE_UNSPECIFIED_LAX_ALLOW_UNSAFE = 'WarnSameSiteUnspecifiedLaxAllowUnsafe';
-	const WARN_SAME_SITE_CROSS_SCHEME_SECURE_URL_METHOD_UNSAFE = 'WarnSameSiteCrossSchemeSecureUrlMethodUnsafe';
-	const WARN_SAME_SITE_CROSS_SCHEME_SECURE_URL_LAX = 'WarnSameSiteCrossSchemeSecureUrlLax';
-	const WARN_SAME_SITE_CROSS_SCHEME_SECURE_URL_STRICT = 'WarnSameSiteCrossSchemeSecureUrlStrict';
-	const WARN_SAME_SITE_CROSS_SCHEME_INSECURE_URL_METHOD_UNSAFE = 'WarnSameSiteCrossSchemeInsecureUrlMethodUnsafe';
-	const WARN_SAME_SITE_CROSS_SCHEME_INSECURE_URL_LAX = 'WarnSameSiteCrossSchemeInsecureUrlLax';
-	const WARN_SAME_SITE_CROSS_SCHEME_INSECURE_URL_STRICT = 'WarnSameSiteCrossSchemeInsecureUrlStrict';
+	const WARN_SAME_SITE_STRICT_LAX_DOWNGRADE_STRICT = 'WarnSameSiteStrictLaxDowngradeStrict';
+	const WARN_SAME_SITE_STRICT_CROSS_DOWNGRADE_STRICT = 'WarnSameSiteStrictCrossDowngradeStrict';
+	const WARN_SAME_SITE_STRICT_CROSS_DOWNGRADE_LAX = 'WarnSameSiteStrictCrossDowngradeLax';
+	const WARN_SAME_SITE_LAX_CROSS_DOWNGRADE_STRICT = 'WarnSameSiteLaxCrossDowngradeStrict';
+	const WARN_SAME_SITE_LAX_CROSS_DOWNGRADE_LAX = 'WarnSameSiteLaxCrossDowngradeLax';
 }
