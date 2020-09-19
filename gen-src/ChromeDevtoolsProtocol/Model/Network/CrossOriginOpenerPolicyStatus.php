4df07011d60bbdd2b6ed5f0d40a807705e628f6d
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2020-09-19 03:01:30 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Network/CrossOriginOpenerPolicyStatus.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Network/CrossOriginOpenerPolicyStatus.php
@@ -14,12 +14,30 @@ final class CrossOriginOpenerPolicyStatus implements \JsonSerializable
 	/** @var string */
 	public $value;
 
+	/** @var string */
+	public $reportOnlyValue;
+
+	/** @var string|null */
+	public $reportingEndpoint;
+
+	/** @var string|null */
+	public $reportOnlyReportingEndpoint;
+
 
 	public static function fromJson($data)
 	{
 		$instance = new static();
 		if (isset($data->value)) {
 			$instance->value = (string)$data->value;
+		}
+		if (isset($data->reportOnlyValue)) {
+			$instance->reportOnlyValue = (string)$data->reportOnlyValue;
+		}
+		if (isset($data->reportingEndpoint)) {
+			$instance->reportingEndpoint = (string)$data->reportingEndpoint;
+		}
+		if (isset($data->reportOnlyReportingEndpoint)) {
+			$instance->reportOnlyReportingEndpoint = (string)$data->reportOnlyReportingEndpoint;
 		}
 		return $instance;
 	}
@@ -30,6 +48,15 @@ final class CrossOriginOpenerPolicyStatus implements \JsonSerializable
 		$data = new \stdClass();
 		if ($this->value !== null) {
 			$data->value = $this->value;
+		}
+		if ($this->reportOnlyValue !== null) {
+			$data->reportOnlyValue = $this->reportOnlyValue;
+		}
+		if ($this->reportingEndpoint !== null) {
+			$data->reportingEndpoint = $this->reportingEndpoint;
+		}
+		if ($this->reportOnlyReportingEndpoint !== null) {
+			$data->reportOnlyReportingEndpoint = $this->reportOnlyReportingEndpoint;
 		}
 		return $data;
 	}
