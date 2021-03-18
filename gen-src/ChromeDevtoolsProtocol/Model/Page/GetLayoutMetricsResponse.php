c39a81dc5f8a11f2d27b930a5cec24954150615d
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2021-03-18 03:01:42 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Page/GetLayoutMetricsResponse.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Page/GetLayoutMetricsResponse.php
@@ -14,21 +14,21 @@ use ChromeDevtoolsProtocol\Model\DOM\Rect;
 final class GetLayoutMetricsResponse implements \JsonSerializable
 {
 	/**
-	 * Deprecated metrics relating to the layout viewport. Can be in DP or in CSS pixels depending on the `enable-use-zoom-for-dsf` flag. Use `normalisedLayoutViewport` instead.
+	 * Deprecated metrics relating to the layout viewport. Can be in DP or in CSS pixels depending on the `enable-use-zoom-for-dsf` flag. Use `cssLayoutViewport` instead.
 	 *
 	 * @var LayoutViewport
 	 */
 	public $layoutViewport;
 
 	/**
-	 * Deprecated metrics relating to the visual viewport. Can be in DP or in CSS pixels depending on the `enable-use-zoom-for-dsf` flag. Use `normalisedVisualViewport` instead.
+	 * Deprecated metrics relating to the visual viewport. Can be in DP or in CSS pixels depending on the `enable-use-zoom-for-dsf` flag. Use `cssVisualViewport` instead.
 	 *
 	 * @var VisualViewport
 	 */
 	public $visualViewport;
 
 	/**
-	 * Deprecated size of scrollable area. Can be in DP or in CSS pixels depending on the `enable-use-zoom-for-dsf` flag. Use `normalisedContentSize` instead.
+	 * Deprecated size of scrollable area. Can be in DP or in CSS pixels depending on the `enable-use-zoom-for-dsf` flag. Use `cssContentSize` instead.
 	 *
 	 * @var Rect
 	 */
