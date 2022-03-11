ef19c7aee5c0d50d9f0133a5c9fa06495908880f
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2022-03-11 03:01:36 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Audits/DeprecationIssueDetails.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Audits/DeprecationIssueDetails.php
@@ -18,13 +18,17 @@ final class DeprecationIssueDetails implements \JsonSerializable
 	public $sourceCodeLocation;
 
 	/**
-	 * The content of the deprecation issue (this won't be translated), e.g. "window.inefficientLegacyStorageMethod will be removed in M97, around January 2022. Please use Web Storage or Indexed Database instead. This standard was abandoned in January, 1970. See https://www.chromestatus.com/feature/5684870116278272 for more details."
+	 * The content of an untranslated deprecation issue, e.g. "window.inefficientLegacyStorageMethod will be removed in M97, around January 2022. Please use Web Storage or Indexed Database instead. This standard was abandoned in January, 1970. See https://www.chromestatus.com/feature/5684870116278272 for more details."
 	 *
 	 * @var string|null
 	 */
 	public $message;
 
-	/** @var string */
+	/**
+	 * The id of an untranslated deprecation issue e.g. PrefixedStorageInfo.
+	 *
+	 * @var string
+	 */
 	public $deprecationType;
 
 
