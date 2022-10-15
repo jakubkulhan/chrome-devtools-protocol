416fe418c12febc7d0ab9440f1459da865cfcbd9
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2022-10-15 03:01:37 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Fetch/ContinueRequestRequest.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Fetch/ContinueRequestRequest.php
@@ -40,7 +40,7 @@ final class ContinueRequestRequest implements \JsonSerializable
 	public $postData;
 
 	/**
-	 * If set, overrides the request headers.
+	 * If set, overrides the request headers. Note that the overrides do not extend to subsequent redirect hops, if a redirect happens. Another override may be applied to a different request produced by a redirect.
 	 *
 	 * @var HeaderEntry[]|null
 	 */
