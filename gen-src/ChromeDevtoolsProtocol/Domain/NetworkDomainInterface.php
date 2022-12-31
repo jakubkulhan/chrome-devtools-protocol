68312ce48175a2b491789f7f7af94b274d4d5c86
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2022-12-31 03:01:55 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Domain/NetworkDomainInterface.php
+++ gen-src/ChromeDevtoolsProtocol/Domain/NetworkDomainInterface.php
@@ -209,7 +209,7 @@ interface NetworkDomainInterface
 
 
 	/**
-	 * Returns all browser cookies. Depending on the backend support, will return detailed cookie information in the `cookies` field.
+	 * Returns all browser cookies. Depending on the backend support, will return detailed cookie information in the `cookies` field. Deprecated. Use Storage.getCookies instead.
 	 *
 	 * @param ContextInterface $ctx
 	 *
