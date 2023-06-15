0a78a8e9a68e7911590fed9df4224ca0b9fa499d
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2023-06-15 03:01:39 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Page/BackForwardCacheNotRestoredReasonEnum.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Page/BackForwardCacheNotRestoredReasonEnum.php
@@ -111,7 +111,6 @@ final class BackForwardCacheNotRestoredReasonEnum
 	public const INDEXED_D_B_EVENT = 'IndexedDBEvent';
 	public const DUMMY = 'Dummy';
 	public const JS_NETWORK_REQUEST_RECEIVED_CACHE_CONTROL_NO_STORE_RESOURCE = 'JsNetworkRequestReceivedCacheControlNoStoreResource';
-	public const WEB_SERIAL = 'WebSerial';
 	public const CONTENT_SECURITY_HANDLER = 'ContentSecurityHandler';
 	public const CONTENT_WEB_AUTHENTICATION_A_P_I = 'ContentWebAuthenticationAPI';
 	public const CONTENT_FILE_CHOOSER = 'ContentFileChooser';
