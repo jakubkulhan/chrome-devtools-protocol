af99338acc9cfe2fd48d5473d2e8b9143db92654
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2024-02-29 03:01:52 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Audits/AttributionReportingIssueTypeEnum.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Audits/AttributionReportingIssueTypeEnum.php
@@ -26,4 +26,9 @@ final class AttributionReportingIssueTypeEnum
 	public const WEB_AND_OS_HEADERS = 'WebAndOsHeaders';
 	public const NO_WEB_OR_OS_SUPPORT = 'NoWebOrOsSupport';
 	public const NAVIGATION_REGISTRATION_WITHOUT_TRANSIENT_USER_ACTIVATION = 'NavigationRegistrationWithoutTransientUserActivation';
+	public const INVALID_INFO_HEADER = 'InvalidInfoHeader';
+	public const NO_REGISTER_SOURCE_HEADER = 'NoRegisterSourceHeader';
+	public const NO_REGISTER_TRIGGER_HEADER = 'NoRegisterTriggerHeader';
+	public const NO_REGISTER_OS_SOURCE_HEADER = 'NoRegisterOsSourceHeader';
+	public const NO_REGISTER_OS_TRIGGER_HEADER = 'NoRegisterOsTriggerHeader';
 }
