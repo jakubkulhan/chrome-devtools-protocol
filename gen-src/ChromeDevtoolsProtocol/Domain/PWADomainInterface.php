b690bf16128a694e7f51c657804e13c745493d1e
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2024-05-02 03:01:54 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Domain/PWADomainInterface.php
+++ gen-src/ChromeDevtoolsProtocol/Domain/PWADomainInterface.php
@@ -31,7 +31,7 @@ interface PWADomainInterface
 
 
 	/**
-	 * Installs the given manifest identity, optionally using the given install_url or IWA bundle location. TODO(crbug.com/337872319) Support IWA to meet the following specific requirement. IWA-specific install description: If the manifest_id is isolated-app://, install_url_or_bundle_url is required, and can be either an http(s) URL or file:// URL pointing to a signed web bundle (.swbn). The .swbn file’s signing key must correspond to manifest_id. If Chrome is not in IWA dev mode, the installation will fail, regardless of the state of the allowlist.
+	 * Installs the given manifest identity, optionally using the given install_url or IWA bundle location. TODO(crbug.com/337872319) Support IWA to meet the following specific requirement. IWA-specific install description: If the manifest_id is isolated-app://, install_url_or_bundle_url is required, and can be either an http(s) URL or file:// URL pointing to a signed web bundle (.swbn). The .swbn file's signing key must correspond to manifest_id. If Chrome is not in IWA dev mode, the installation will fail, regardless of the state of the allowlist.
 	 *
 	 * @param ContextInterface $ctx
 	 * @param InstallRequest $request
