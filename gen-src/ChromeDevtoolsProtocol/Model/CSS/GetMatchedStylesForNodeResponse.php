27c8179cefd6880d10e43a1aab4c34f97ca5124f
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2024-07-06 03:02:04 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/CSS/GetMatchedStylesForNodeResponse.php
+++ gen-src/ChromeDevtoolsProtocol/Model/CSS/GetMatchedStylesForNodeResponse.php
@@ -68,7 +68,7 @@ final class GetMatchedStylesForNodeResponse implements \JsonSerializable
 	public $cssPositionFallbackRules;
 
 	/**
-	 * A list of CSS @position-try rules matching this node, based on the position-try-options property.
+	 * A list of CSS @position-try rules matching this node, based on the position-try-fallbacks property.
 	 *
 	 * @var CSSPositionTryRule[]|null
 	 */
