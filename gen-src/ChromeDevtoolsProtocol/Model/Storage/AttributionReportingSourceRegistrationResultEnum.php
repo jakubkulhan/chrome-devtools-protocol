af3eff9cb019b2ccc314b809ead543694a071020
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2024-08-29 03:01:54 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Storage/AttributionReportingSourceRegistrationResultEnum.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Storage/AttributionReportingSourceRegistrationResultEnum.php
@@ -23,6 +23,8 @@ final class AttributionReportingSourceRegistrationResultEnum
 	public const DESTINATION_BOTH_LIMITS_REACHED = 'destinationBothLimitsReached';
 	public const REPORTING_ORIGINS_PER_SITE_LIMIT_REACHED = 'reportingOriginsPerSiteLimitReached';
 	public const EXCEEDS_MAX_CHANNEL_CAPACITY = 'exceedsMaxChannelCapacity';
+	public const EXCEEDS_MAX_SCOPES_CHANNEL_CAPACITY = 'exceedsMaxScopesChannelCapacity';
 	public const EXCEEDS_MAX_TRIGGER_STATE_CARDINALITY = 'exceedsMaxTriggerStateCardinality';
+	public const EXCEEDS_MAX_EVENT_STATES_LIMIT = 'exceedsMaxEventStatesLimit';
 	public const DESTINATION_PER_DAY_REPORTING_LIMIT_REACHED = 'destinationPerDayReportingLimitReached';
 }
