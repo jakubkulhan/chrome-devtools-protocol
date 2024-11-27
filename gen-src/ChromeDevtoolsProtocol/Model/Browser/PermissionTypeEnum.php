057f1917335e5241cf8bd0e8afea337af7dd58b9
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2024-11-27 03:01:58 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Browser/PermissionTypeEnum.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Browser/PermissionTypeEnum.php
@@ -11,18 +11,21 @@ namespace ChromeDevtoolsProtocol\Model\Browser;
  */
 final class PermissionTypeEnum
 {
-	public const ACCESSIBILITY_EVENTS = 'accessibilityEvents';
+	public const AR = 'ar';
 	public const AUDIO_CAPTURE = 'audioCapture';
-	public const BACKGROUND_SYNC = 'backgroundSync';
+	public const AUTOMATIC_FULLSCREEN = 'automaticFullscreen';
 	public const BACKGROUND_FETCH = 'backgroundFetch';
+	public const BACKGROUND_SYNC = 'backgroundSync';
+	public const CAMERA_PAN_TILT_ZOOM = 'cameraPanTiltZoom';
 	public const CAPTURED_SURFACE_CONTROL = 'capturedSurfaceControl';
 	public const CLIPBOARD_READ_WRITE = 'clipboardReadWrite';
 	public const CLIPBOARD_SANITIZED_WRITE = 'clipboardSanitizedWrite';
 	public const DISPLAY_CAPTURE = 'displayCapture';
 	public const DURABLE_STORAGE = 'durableStorage';
-	public const FLASH = 'flash';
 	public const GEOLOCATION = 'geolocation';
+	public const HAND_TRACKING = 'handTracking';
 	public const IDLE_DETECTION = 'idleDetection';
+	public const KEYBOARD_LOCK = 'keyboardLock';
 	public const LOCAL_FONTS = 'localFonts';
 	public const MIDI = 'midi';
 	public const MIDI_SYSEX = 'midiSysex';
@@ -30,15 +33,18 @@ final class PermissionTypeEnum
 	public const NOTIFICATIONS = 'notifications';
 	public const PAYMENT_HANDLER = 'paymentHandler';
 	public const PERIODIC_BACKGROUND_SYNC = 'periodicBackgroundSync';
+	public const POINTER_LOCK = 'pointerLock';
 	public const PROTECTED_MEDIA_IDENTIFIER = 'protectedMediaIdentifier';
 	public const SENSORS = 'sensors';
-	public const STORAGE_ACCESS = 'storageAccess';
+	public const SMART_CARD = 'smartCard';
 	public const SPEAKER_SELECTION = 'speakerSelection';
+	public const STORAGE_ACCESS = 'storageAccess';
 	public const TOP_LEVEL_STORAGE_ACCESS = 'topLevelStorageAccess';
 	public const VIDEO_CAPTURE = 'videoCapture';
-	public const VIDEO_CAPTURE_PAN_TILT_ZOOM = 'videoCapturePanTiltZoom';
+	public const VR = 'vr';
 	public const WAKE_LOCK_SCREEN = 'wakeLockScreen';
 	public const WAKE_LOCK_SYSTEM = 'wakeLockSystem';
 	public const WEB_APP_INSTALLATION = 'webAppInstallation';
+	public const WEB_PRINTING = 'webPrinting';
 	public const WINDOW_MANAGEMENT = 'windowManagement';
 }
