bc80874910f4106fe4c0080d8522caf341d49af1
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2025-10-03 03:02:14 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Network/SetBlockedURLsRequest.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Network/SetBlockedURLsRequest.php
@@ -12,9 +12,16 @@ namespace ChromeDevtoolsProtocol\Model\Network;
 final class SetBlockedURLsRequest implements \JsonSerializable
 {
 	/**
+	 * URL patterns to block. Patterns use the URLPattern constructor string syntax (https://urlpattern.spec.whatwg.org/). Example: `*://*:* /*.css`.
+	 *
+	 * @var string[]|null
+	 */
+	public $urlPatterns;
+
+	/**
 	 * URL patterns to block. Wildcards ('*') are allowed.
 	 *
-	 * @var string[]
+	 * @var string[]|null
 	 */
 	public $urls;
 
@@ -26,6 +33,12 @@ final class SetBlockedURLsRequest implements \JsonSerializable
 	public static function fromJson($data)
 	{
 		$instance = new static();
+		if (isset($data->urlPatterns)) {
+			$instance->urlPatterns = [];
+			foreach ($data->urlPatterns as $item) {
+				$instance->urlPatterns[] = (string)$item;
+			}
+		}
 		if (isset($data->urls)) {
 			$instance->urls = [];
 			foreach ($data->urls as $item) {
@@ -39,6 +52,12 @@ final class SetBlockedURLsRequest implements \JsonSerializable
 	public function jsonSerialize()
 	{
 		$data = new \stdClass();
+		if ($this->urlPatterns !== null) {
+			$data->urlPatterns = [];
+			foreach ($this->urlPatterns as $item) {
+				$data->urlPatterns[] = $item;
+			}
+		}
 		if ($this->urls !== null) {
 			$data->urls = [];
 			foreach ($this->urls as $item) {
@@ -57,5 +76,16 @@ final class SetBlockedURLsRequest implements \JsonSerializable
 	public static function builder(): SetBlockedURLsRequestBuilder
 	{
 		return new SetBlockedURLsRequestBuilder();
+	}
+
+
+	/**
+	 * Create new empty instance.
+	 *
+	 * @return self
+	 */
+	public static function make(): self
+	{
+		return static::builder()->build();
 	}
 }
--- gen-src/ChromeDevtoolsProtocol/Model/Network/SetBlockedURLsRequestBuilder.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Network/SetBlockedURLsRequestBuilder.php
@@ -2,8 +2,6 @@
 
 namespace ChromeDevtoolsProtocol\Model\Network;
 
-use ChromeDevtoolsProtocol\Exception\BuilderException;
-
 /**
  * @generated This file has been auto-generated, do not edit.
  *
@@ -11,6 +9,7 @@ use ChromeDevtoolsProtocol\Exception\BuilderException;
  */
 final class SetBlockedURLsRequestBuilder
 {
+	private $urlPatterns;
 	private $urls;
 
 
@@ -20,16 +19,26 @@ final class SetBlockedURLsRequestBuilder
 	public function build(): SetBlockedURLsRequest
 	{
 		$instance = new SetBlockedURLsRequest();
-		if ($this->urls === null) {
-			throw new BuilderException('Property [urls] is required.');
-		}
+		$instance->urlPatterns = $this->urlPatterns;
 		$instance->urls = $this->urls;
 		return $instance;
 	}
 
 
 	/**
-	 * @param string[] $urls
+	 * @param string[]|null $urlPatterns
+	 *
+	 * @return self
+	 */
+	public function setUrlPatterns($urlPatterns): self
+	{
+		$this->urlPatterns = $urlPatterns;
+		return $this;
+	}
+
+
+	/**
+	 * @param string[]|null $urls
 	 *
 	 * @return self
 	 */
