68f85599b424908ef1f8e871eaae77875dba39d1
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2025-11-02 03:02:11 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Network/PrivateNetworkRequestPolicyEnum.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Network/PrivateNetworkRequestPolicyEnum.php
@@ -14,8 +14,6 @@ final class PrivateNetworkRequestPolicyEnum
 	public const ALLOW = 'Allow';
 	public const BLOCK_FROM_INSECURE_TO_MORE_PRIVATE = 'BlockFromInsecureToMorePrivate';
 	public const WARN_FROM_INSECURE_TO_MORE_PRIVATE = 'WarnFromInsecureToMorePrivate';
-	public const PREFLIGHT_BLOCK = 'PreflightBlock';
-	public const PREFLIGHT_WARN = 'PreflightWarn';
 	public const PERMISSION_BLOCK = 'PermissionBlock';
 	public const PERMISSION_WARN = 'PermissionWarn';
 }
