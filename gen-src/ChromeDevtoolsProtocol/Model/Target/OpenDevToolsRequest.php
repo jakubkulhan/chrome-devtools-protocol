47436d2282b5cb7d4dc8c177232eecb31287b9ae
Jakub Kulhan <jakub.kulhan+circleci@gmail.com>
2026-05-30 03:01:58 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Model/Target/OpenDevToolsRequest.php
+++ gen-src/ChromeDevtoolsProtocol/Model/Target/OpenDevToolsRequest.php
@@ -19,7 +19,7 @@ final class OpenDevToolsRequest implements \JsonSerializable
 	public $targetId;
 
 	/**
-	 * The id of the panel we want DevTools to open initially. Currently supported panels are elements, console, network, sources, resources and performance.
+	 * The id of the panel we want DevTools to open initially. Currently supported panels are elements, console, network, sources, resources, timeline, chrome-recorder, heap-profiler, lighthouse, and security.
 	 *
 	 * @var string|null
 	 */
